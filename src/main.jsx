b3ba6c8daa41bfb88345fb115d0253a15512ac39
NifatHossain <nifathossain1480@gmail.com>
2024-07-16 07:03:49 +0600

    Added errorPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+    errorElement: <ErrorPage></ErrorPage>,
   },
 ]);
 
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="flex justify-center min-h-screen items-center">
+            <div>
+                <h2>Sorry page not found</h2>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
