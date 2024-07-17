7033fd1f80a72edb6e0c7ac595e80d5526852316
NifatHossain <nifathossain1480@gmail.com>
2024-07-17 22:41:02 +0600

    Added many routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,13 @@ import AuthProvider from './provider/AuthProvider';
 import Login from './pages/Login';
 import PrivateRoute from './provider/PrivateRoute';
 import PendingPage from './pages/PendingPage';
+import CashIn from './pages/CashIn';
+import CashOut from './pages/CashOut';
+import UserHistory from './pages/UserHistory';
+import AgentHistory from './pages/AgentHistory';
+import SendMoney from './pages/SendMoney';
+import AgentManagement from './pages/AgentManagement';
+import UserManagement from './pages/UserManagement';
 
 const router = createBrowserRouter([
   {
@@ -38,8 +45,33 @@ const router = createBrowserRouter([
       },
       {
         path:'/cashin',
-        element:<PrivateRoute></PrivateRoute>
+        element:<PrivateRoute><CashIn></CashIn></PrivateRoute>
+      },
+      {
+        path:'/cashout',
+        element:<PrivateRoute><CashOut></CashOut></PrivateRoute>
+      },
+      {
+        path:'/sendmoney',
+        element: <PrivateRoute><SendMoney></SendMoney></PrivateRoute>
+      },
+      {
+        path:'/userhistory',
+        element:<PrivateRoute><UserHistory></UserHistory></PrivateRoute>
+      },
+      {
+        path:'/agenthistory',
+        element:<PrivateRoute><AgentHistory></AgentHistory></PrivateRoute>
+      },
+      {
+        path:'/agentmanagement',
+        element:<AgentManagement></AgentManagement>
+      },
+      {
+        path:'/usermanagement',
+        element:<UserManagement></UserManagement>
       }
+
     ]
   },
 ]);
---
+++ src/pages/UserManagement.jsx
@@ -0,0 +1,11 @@
+
+
+const UserManagement = () => {
+    return (
+        <div>
+            <p>Information of all users</p>
+        </div>
+    );
+};
+
+export default UserManagement;
